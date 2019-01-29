e46bbf95bbae639b45ee9794aecd440ae96f13b2
Noam Silver <noamsilver@gmail.com>
2019-01-29 21:48:59 +0200

    Updated wording

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const chatData = [];
 
 io.on('connection', (socket) => {
-  console.log('a user is connected')
+  console.log('a user connected')
   socket.on('disconnect', () => {
     console.log('a user disconnected')
   })
